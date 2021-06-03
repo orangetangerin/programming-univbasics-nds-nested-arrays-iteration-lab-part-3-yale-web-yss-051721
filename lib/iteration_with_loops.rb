baa1cf3020efdbb5c88bcab93dbecbca86e318a5
Jonathan Yu <jonathan.yu.jwy2@yale.edu>
2021-06-03 00:42:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,6 +2,7 @@ def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
   i = 0
+  strings = []
   while i < src.length do
     j = 0
     while j < src[i].length do
